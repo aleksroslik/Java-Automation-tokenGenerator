85848cbf61b0e0c6114c23db3db68f7e2cfca645
JavaClass <aleksroslik@gmail.com>
2022-08-08 11:17:43 +0200

    improved finished task

--- src/main/java/TokenGenerator.java
+++ src/main/java/TokenGenerator.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 public class TokenGenerator {
     public static void main(String[] args) {
         String charTable = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
-        StringBuilder token = new StringBuilder();
         Random random = new Random();
+        StringBuilder token = new StringBuilder();
+
 
         Scanner input = new Scanner(System.in);
         System.out.println("Which token would you like to generate? \n Enter 5, for 5-character token, " +
@@ -13,35 +14,14 @@ public class TokenGenerator {
         int userInput = input.nextInt();
 
         switch (userInput) {
-            case 5 -> {
-                int length = 5;
-                for(int i = 0; i<length; i++) {
+            case 5, 10, 15 -> {
+                for(int i = 0; i<userInput; i++) {
                     int index = random.nextInt(charTable.length());
                     char randomChar = charTable.charAt(index);
                     token.append(randomChar);
                 }
-                String shortToken = token.toString();
-                System.out.println("Your token is: " + shortToken);
-            }
-            case 10 -> {
-                int length = 10;
-                for(int i = 0; i<length; i++) {
-                    int index = random.nextInt(charTable.length());
-                    char randomChar = charTable.charAt(index);
-                    token.append(randomChar);
-                }
-                String mediumToken = token.toString();
-                System.out.println("Your token is: " + mediumToken);
-            }
-            case 15 -> {
-                int length = 15;
-                for(int i = 0; i<length; i++) {
-                    int index = random.nextInt(charTable.length());
-                    char randomChar = charTable.charAt(index);
-                    token.append(randomChar);
-                }
-                String longToken = token.toString();
-                System.out.println("Your token is: " + longToken);
+                String generatedToken = token.toString();
+                System.out.println("Your token is: " + generatedToken);
             }
             default -> System.out.println("Please try again - enter 5, 10 or 15 - to get your token");
         }
