1e95e30eac71dee92030e7a23162a5158d749a69
JavaClass <aleksroslik@gmail.com>
2022-08-09 07:10:18 +0200

    final fix

--- src/main/java/TokenGenerator.java
+++ src/main/java/TokenGenerator.java
@@ -3,22 +3,20 @@ import java.util.Scanner;
 
 public class TokenGenerator {
     public static void main(String[] args) {
-        String charTable = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+
         Random random = new Random();
         StringBuilder token = new StringBuilder();
-
-
         Scanner input = new Scanner(System.in);
+
         System.out.println("Which token would you like to generate? \n Enter 5, for 5-character token, " +
-                           "\n Enter 10, for 10-character token, \n Enter 15, for 15-character token.");
+                "\n Enter 10, for 10-character token, \n Enter 15, for 15-character token.");
         int userInput = input.nextInt();
 
         switch (userInput) {
             case 5, 10, 15 -> {
-                for(int i = 0; i<userInput; i++) {
-                    int index = random.nextInt(charTable.length());
-                    char randomChar = charTable.charAt(index);
-                    token.append(randomChar);
+                for (int i = 0; i < userInput; i++) {
+                    char charTable = (char) (random.nextInt(126 - 33) + 33);
+                    token.append(charTable);
                 }
                 String generatedToken = token.toString();
                 System.out.println("Your token is: " + generatedToken);
